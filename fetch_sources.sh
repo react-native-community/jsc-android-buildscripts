fd479ac51d2858cc4fe8f69fb4d209f1ba8002e1
Daniel Zlotin <danielzl@wix.com>
2017-11-27 14:29:20 +0200

    dynamic version values

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+# set version for gradle artifacts
+VERSION_NAME=`echo ${npm_package_config_webkitGTK} | sed -e "s/\./_/g"`
+sed -i.bak "s/VERSION_NAME=.*/VERSION_NAME=${VERSION_NAME}/" lib/lib/gradle.properties lib/libIntl/gradle.properties
+rm lib/lib/gradle.properties.bak lib/libIntl/gradle.properties.bak
+
+# download sources
 TARGET_DIR=target
-SVN_DIR=releases/WebKitGTK/webkit-2.18.2
+SVN_DIR="releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}"
 
 mkdir -p $TARGET_DIR/webkit
 svn export https://svn.webkit.org/repository/webkit/$SVN_DIR/Source $TARGET_DIR/webkit/Source
@@ -10,4 +16,4 @@ svn export https://svn.webkit.org/repository/webkit/$SVN_DIR/CMakeLists.txt $TAR
 
 mkdir -p $TARGET_DIR/icu
 # This is for the latest release for the latest android from https://android.googlesource.com/platform/external/icu/
-curl https://android.googlesource.com/platform/external/icu/+archive/android-8.0.0_r24/icu4c.tar.gz | tar xzf - -C $TARGET_DIR/icu
+curl "https://android.googlesource.com/platform/external/icu/+archive/android-${npm_package_config_android_icu}/icu4c.tar.gz" | tar xzf - -C $TARGET_DIR/icu
