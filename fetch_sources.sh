ea04b4c466b80f6182dd31f0e1105f687ceb0b0c
Daniel Zlotin <danielzl@wix.com>
2017-12-11 11:21:21 +0200

    versioning

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # set version for gradle artifacts
-VERSION_NAME=`${npm_package_config_webkitGTK}-${npm_package_config_android_icu}-${npm_package_config_name}`
+VERSION_NAME="${npm_package_config_webkitGTK}-${npm_package_config_android_icu}-${npm_package_config_name}"
 sed -i.bak "s/VERSION_NAME=.*/VERSION_NAME=${VERSION_NAME}/" lib/lib/gradle.properties lib/libIntl/gradle.properties
 rm lib/lib/gradle.properties.bak lib/libIntl/gradle.properties.bak
 
