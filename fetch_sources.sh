62112daaec77c7d671590350777e5b530beeb7d9
Daniel Zlotin <danielzl@wix.com>
2017-12-11 11:16:04 +0200

    recompile tip

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # set version for gradle artifacts
-VERSION_NAME=`echo ${npm_package_config_webkitGTK} | sed -e "s/\./_/g"`
+VERSION_NAME=`${npm_package_config_webkitGTK}-${npm_package_config_android_icu}-${npm_package_config_name}`
 sed -i.bak "s/VERSION_NAME=.*/VERSION_NAME=${VERSION_NAME}/" lib/lib/gradle.properties lib/libIntl/gradle.properties
 rm lib/lib/gradle.properties.bak lib/libIntl/gradle.properties.bak
 
--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-./icu-prep.sh
-./jsc-prep.sh
 ./all.sh
 cd lib
 ./gradlew clean lib:installArchives
