014a37bb9c928e245d2aeebe9b6d905f175d5353
Daniel Zlotin <danielzl@wix.com>
2018-01-31 12:15:33 +0200

    update

--- all.sh
+++ all.sh
@@ -10,7 +10,7 @@ compile_arch() {
 }
 
 compile() {
-  for arch in arm #x86
+  for arch in arm x86
   do
     export JSC_ARCH=$arch
     export ANDROID_API=21
@@ -18,20 +18,25 @@ compile() {
     compile_arch
   done
 
-  for arch in arm64 #x86_64
-  do
-    export JSC_ARCH=$arch
-    export ANDROID_API=21
-    export ENABLE_COMPAT=0
-    compile_arch
-  done
+  if ${npm_package_config_x64}
+  then
+    for arch in arm64 x86_64
+    do
+      export JSC_ARCH=$arch
+      export ANDROID_API=21
+      export ENABLE_COMPAT=0
+      compile_arch
+    done
+  fi
 }
 
 export FLAVOR=no-intl
 export ENABLE_INTL=0
 compile
 
-
-# export FLAVOR=intl
-# export ENABLE_INTL=1
-# compile
+if ${npm_package_config_i18n}
+then
+  export FLAVOR=intl
+  export ENABLE_INTL=1
+  compile
+fi
--- fetch_sources.sh
+++ fetch_sources.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-# set version for gradle artifacts
-VERSION_NAME="${npm_package_config_webkitGTK}-${npm_package_config_android_icu}-${npm_package_config_name}"
-sed -i.bak "s/VERSION_NAME=.*/VERSION_NAME=${VERSION_NAME}/" lib/lib/gradle.properties lib/libIntl/gradle.properties
-rm lib/lib/gradle.properties.bak lib/libIntl/gradle.properties.bak
-
 # download sources
 TARGET_DIR=target
 SVN_DIR="releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}"
