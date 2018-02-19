0d201ddf32666604a8dac4efbcd6c19defb4e3c9
Daniel Zlotin <zlotindaniel@gmail.com>
2018-02-19 17:51:12 +0200

    fix zip

--- start.sh
+++ start.sh
@@ -5,10 +5,9 @@ source ./calc-version.sh
 ./all.sh
 cd lib
 ./gradlew clean lib:installArchives
-cd ..
 
-zip "$VERSION_NAME" "lib/android/**/*"
+cd android
+zip -r "../../result/$VERSION_NAME.zip" "."
 
 printf "\n\n\n\n\n\t\t\tCompiled Version: ${VERSION_NAME}\n\n\n\n\n\n"
-say -v Carmit \"I'm not slacking off, my code's compiling\"
-say "$VERSION_NAME"
+say -v Carmit "I am not slacking off, my code's compiling"
