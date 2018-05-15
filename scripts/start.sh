9f7e373f64116f3b4225796a80fa77561e22b56e
Daniel Zlotin <zlotindaniel@gmail.com>
2018-05-15 19:50:02 +0300

    compiling separately

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,19 +6,14 @@ VERSION_NAME=$($ROOTDIR/scripts/version.sh)
 $ROOTDIR/scripts/compile/all.sh
 
 cd $ROOTDIR/lib
-if ${npm_package_config_i18n}
-then
-  ./gradlew clean libIntl:installArchives
-else
-  ./gradlew clean lib:installArchives
-fi
+./gradlew clean createAAR --project-prop VERSION_NAME="$VERSION_NAME"
 
-cd $ROOTDIR/lib/android
-zip -r "../../dist/$VERSION_NAME.zip" "."
+# cd $ROOTDIR/lib/android
+# zip -r "../../dist/$VERSION_NAME.zip" "."
 
-cd $ROOTDIR
-git add -A
-git commit -m "compiled $VERSION_NAME"
+# cd $ROOTDIR
+# git add -A
+# git commit -m "compiled $VERSION_NAME"
 
 printf "\n\n\n\n\n\t\t\tCompiled Version: ${VERSION_NAME}\n\n\n\n\n\n"
 say -v Carmit "I am not slacking off, my code is compiling."
