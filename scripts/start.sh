3146a169c548c885c45839c67fd822476185a87b
Daniel Zlotin <zlotindaniel@gmail.com>
2018-05-16 17:29:33 +0300

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,20 +3,14 @@
 ROOTDIR=$PWD
 VERSION_NAME=$($ROOTDIR/scripts/version.sh)
 
+# compile
 rm -rf $ROOTDIR/build/compiled
-
 $ROOTDIR/scripts/compile/all.sh
 
+# create aar
 cd $ROOTDIR/lib
 ./gradlew clean createAAR --project-prop VERSION_NAME="$VERSION_NAME"
+cd $ROOTDIR
 
-# cd $ROOTDIR/lib/android
-# zip -r "../../dist/$VERSION_NAME.zip" "."
-
-# cd $ROOTDIR
-# git add -A
-# git commit -m "compiled $VERSION_NAME"
-
-printf "\n\n\n\n\n\t\t\tCompiled Version: ${VERSION_NAME}\n\n\n\n\n\n"
+printf "\n\n\n\n\n\t\t\tCompiled Version: \x1B[32m${VERSION_NAME}\x1B[0m\n\n\n\n\n\n"
 say -v Carmit "I am not slacking off, my code is compiling."
-say -v Carmit "${VERSION_NAME}"
