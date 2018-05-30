c7f7f2a576b8977cdef6476b66ef9d3e06915bdc
Daniel Zlotin <zlotindaniel@gmail.com>
2018-05-30 13:06:38 +0300

    update results

--- scripts/revision.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-svn info --show-item last-changed-revision "https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}/"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
 ROOTDIR=$PWD
-REVISION=$($ROOTDIR/scripts/revision.sh)
+REVISION=$(svn info --show-item last-changed-revision "https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}")
+INFO=$(svn info "https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}")
 CONFIG=$(node -e "console.log(require('./package.json').config)")
 
 # compile
@@ -13,5 +14,5 @@ cd $ROOTDIR/lib
 ./gradlew clean createAAR --project-prop revision="$REVISION" --project-prop i18n="$npm_package_config_i18n"
 cd $ROOTDIR
 
-printf "\n\n\n\n\n\t\t\tCompiled Version: \x1B[32m$REVISION\x1B[0m\n\n\n\nconfig:\n$CONFIG\n\n\n\n"
+printf "\n\n\n\n\n\t\t\tCompiled Version: \x1B[32m$REVISION\x1B[0m\n\n\n\nconfig:\n$CONFIG\n\ninfo:\n$INFO\n\n"
 say -v Carmit "I am not slacking off, my code's compiling."
