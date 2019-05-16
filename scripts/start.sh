62641162ccd181ad6e982bfc9bdabb65552654ed
Kudo Chien <ckchien@gmail.com>
2019-05-16 18:44:31 +0800

    Introduce patch management (#103)
    Summary:
        Instead of put all things in one jsc.patch which is hard to manage.
        This commit separate patches by purpose and add comment in patch.sh
        to describe each patch purpose.

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,12 +6,13 @@ export ROOTDIR=$PWD
 export TARGETDIR=$ROOTDIR/build/target
 source $ROOTDIR/scripts/info.sh
 export JSC_VERSION=${npm_package_version}
+SCRIPT_DIR=$(cd `dirname $0`; pwd)
 
 patchAndMakeICU() {
   printf "\n\n\t\t===================== patch and make icu into target/icu/host =====================\n\n"
   ICU_VERSION_MAJOR="$(awk '/ICU_VERSION_MAJOR_NUM/ {print $3}' $TARGETDIR/icu/source/common/unicode/uvernum.h)"
   printf "ICU version: ${ICU_VERSION_MAJOR}\n"
-  patch -d $TARGETDIR -p1 < $ROOTDIR/patches/icu.patch
+  $SCRIPT_DIR/patch.sh icu
 
   # use compiled .dat archive from Android Chromium
   cp $TARGETDIR/icu/android/icudtl.dat $TARGETDIR/icu/source/data/in/icudt${ICU_VERSION_MAJOR}l.dat
@@ -39,13 +40,7 @@ patchAndMakeICU() {
 
 patchJsc() {
   printf "\n\n\t\t===================== patch jsc =====================\n\n"
-  patch -d $TARGETDIR -p1 < $ROOTDIR/patches/jsc.patch
-
-  # disable i18n for non-i18n build
-  if [[ "$I18N" = false ]]
-  then
-    patch -d $TARGETDIR -N -p1 < $ROOTDIR/patches/intl/icu-disabled.patch
-  fi
+  $SCRIPT_DIR/patch.sh jsc
 }
 
 prep() {
