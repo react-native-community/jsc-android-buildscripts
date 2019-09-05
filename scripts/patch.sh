36836044b4d592f2f23ab6a84686a91a8cffe68b
Kudo Chien <ckchien@gmail.com>
2019-09-06 02:04:38 +0800

    Fix Intl default timezone is incorrect

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -55,6 +55,9 @@ JSC_PATCHSET=(
 
   # Workaround JIT crash on arm64, especially for Saumsung S7 Edge
   "jsc_fix_arm64_jit_crash.patch"
+
+  # Intl default timezone with Android integration
+  "jsc_intl_timezone.patch"
 )
 
 if [[ "$I18N" = false ]]
