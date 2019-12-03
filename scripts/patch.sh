fbe7e24367d4dc18affb0036cb478daf53a08684
mustafa <971530+tosbaha@users.noreply.github.com>
2019-12-03 13:44:28 +0300

    GC concurrent issue potential fix

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -57,6 +57,9 @@ JSC_PATCHSET=(
 
   # Improve heap GC mechanism like iOS
   "jsc_heap_gc_like_ios.patch"
+  
+  # GC concurrent issue potential fix
+  "jsc_fix_concurrent_gc_issue.patch"
 )
 
 if [[ "$I18N" = false ]]
