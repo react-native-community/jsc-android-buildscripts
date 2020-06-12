ef2df99de4e12066ce80f3636fa69abdc18a94c0
KAMEDAkyosuke <organlounge@gmail.com>
2020-06-12 18:38:39 +0900

    force set to en for get REVISION

--- scripts/info.sh
+++ scripts/info.sh
@@ -4,7 +4,7 @@ URL="https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-${npm_pa
 ROOTDIR=$PWD
 
 INFO=$(svn info "${URL}")
-export REVISION=$(svn info "${URL}" | sed -n 's/^Last Changed Rev: //p')
+export REVISION=$(LANG=en svn info "${URL}" | sed -n 's/^Last Changed Rev: //p')
 CONFIG=$(node -e "console.log(require('$ROOTDIR/package.json').config)")
 APPLE_VERSION=$(svn cat "${URL}/Source/WebCore/Configurations/Version.xcconfig" | grep 'MAJOR_VERSION\s=\|MINOR_VERSION\s=\|TINY_VERSION\s=\|MICRO_VERSION\s=\|NANO_VERSION\s=')
 
