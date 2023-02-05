61ac7a8fe5ed667b1987b0ece770e4a0f71f8941
Kudo Chien <ckchien@gmail.com>
2023-02-05 10:11:46 +0800

    feat: build with ndk r23c on top of webkitgtk 2.26.1 (#179)
    # Why
    
    fixes https://github.com/react-native-community/jsc-android-buildscripts/issues/178
    
    # How
    
    - manually pick necessary changes for ndk r23 from #169. it's ndk r23c which is newer than react-native 0.71's r23b. i cannot build jsc on top on r23b successfully. hopefully r23b <-> r23c is good for abi safety.
    - fix build errors for webkitgtk 2.26.1 + ndk r23.
    - unlike #169, this pr is still using shared c++ runtime.
    - update test project to 0.71 and build from source code because we need a patch to link libunwind in libjscexecutor.so.
    
    # Test Plan
    
    ci passed

--- scripts/info.sh
+++ scripts/info.sh
@@ -4,7 +4,8 @@ URL="https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-${npm_pa
 ROOTDIR=$PWD
 
 INFO=$(svn info "${URL}")
-export REVISION=$(LANG=en svn info "${URL}" | sed -n 's/^Last Changed Rev: //p')
+export REVISION=$(node -e "console.log(require('./package.json').version.split('.')[0])")
+WEBKIT_REVISION=$(LANG=en svn info "${URL}" | sed -n 's/^Last Changed Rev: //p')
 CONFIG=$(node -e "console.log(require('$ROOTDIR/package.json').config)")
 APPLE_VERSION=$(svn cat "${URL}/Source/WebCore/Configurations/Version.xcconfig" | grep 'MAJOR_VERSION\s=\|MINOR_VERSION\s=\|TINY_VERSION\s=\|MICRO_VERSION\s=\|NANO_VERSION\s=')
 
@@ -15,6 +16,7 @@ else
 fi
 
 printf "\n\n\n\n\n\t\t\tRevision: \x1B[32m$REVISION\x1B[0m\n\n\n"
+printf "WebKit revision:\n$WEBKIT_REVISION\n\n"
 printf "Config:\n$CONFIG\n\n"
 printf "Info:\n$INFO\n\n"
 printf "AppleWebkit:\n$APPLE_VERSION\n\n"
