dfb3b5f4d23a49e2c4941e5af0ae9aeff549f106
Kudo Chien <ckchien@gmail.com>
2024-11-16 18:33:29 +0800

    Update build scripts
    - migrate repo from svn to git
    - since we bump ndk to r27, then we can remove deprecated NDK patches
    - set minSdkVersion to 24

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,12 +3,11 @@
 # download sources
 rm -rf $PWD/build
 TARGET_DIR=$PWD/build/download
-SVN_URL="releases/WebKitGTK/webkit-${npm_package_config_webkitGTK}"
+REPO_URL="https://github.com/WebKit/WebKit.git"
 
-mkdir -p $TARGET_DIR/webkit
-svn export https://svn.webkit.org/repository/webkit/$SVN_URL/Source $TARGET_DIR/webkit/Source
-svn export https://svn.webkit.org/repository/webkit/$SVN_URL/Tools $TARGET_DIR/webkit/Tools
-svn export https://svn.webkit.org/repository/webkit/$SVN_URL/CMakeLists.txt $TARGET_DIR/webkit/CMakeLists.txt
+mkdir -p $TARGET_DIR
+# FIXME: hardcoded branch because WebKitGTK 2.26 does not have tag
+git clone $REPO_URL --branch 'webkitgtk/2.26' --depth 1 $TARGET_DIR/webkit
 
 mkdir -p $TARGET_DIR/icu
 curl "https://chromium.googlesource.com/chromium/deps/icu/+archive/${npm_package_config_chromiumICUCommit}.tar.gz" | tar xzf - -C $TARGET_DIR/icu
